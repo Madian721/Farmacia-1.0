41a462e419c278c6081e97841321f98a75d918a4
Madian721 <ttperez915@gmail.com>
2026-03-18 20:42:25 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
+let carrito = 0;
+
 function comprar(producto){
 
-alert("Agregaste " + producto + " al carrito 🛒");
+carrito++;
+
+document.getElementById("contador").textContent = carrito;
+
+alert(producto + " agregado al carrito");
+
+}
+
+function buscarProducto(){
+
+let input = document.getElementById("buscar").value.toLowerCase();
+
+let productos = document.getElementsByClassName("producto");
+
+for(let i=0;i<productos.length;i++){
+
+let nombre = productos[i].getElementsByTagName("h3")[0].textContent.toLowerCase();
+
+if(nombre.includes(input)){
+productos[i].style.display="block";
+}else{
+productos[i].style.display="none";
+}
+
+}
 
 }
