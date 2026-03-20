65ba6d2eb5c38b3501f21a92c24ad4a5070b9f2d
Madian721 <ttperez915@gmail.com>
2026-03-19 20:34:10 -0600

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.getElementById("buscar").addEventListener("keyup", function(){
 let filtro = this.value.toLowerCase();
 let productos = document.querySelectorAll(".producto");
 
-productos.forEach(p => {
+productos.forEach(p=>{
 let texto = p.innerText.toLowerCase();
 p.style.display = texto.includes(filtro) ? "block" : "none";
 });
@@ -22,12 +22,3 @@ e.preventDefault();
 alert("Mensaje enviado correctamente");
 });
 
-/* AUTO SCROLL OFERTAS */
-let carrusel = document.getElementById("carrusel");
-
-setInterval(()=>{
-carrusel.scrollLeft += 250;
-if(carrusel.scrollLeft >= carrusel.scrollWidth - carrusel.clientWidth){
-carrusel.scrollLeft = 0;
-}
-},3000);
